696347d7957c38c17d738b471f929ed63adbb85c
Rovshan Musayev <arlington1985@gmail.com>
2019-09-01 18:34:03 +0200

    Fix password param

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sudo mkdir wphotos
-sudo mount.cifs //192.168.0.244/wphotos wphotos/ -o user=wphotos pass=$wphotos_password
+sudo mount.cifs //192.168.0.244/wphotos wphotos/ -o user=wphotos,pass=$wphotos_password
 sudo docker-compose up -d --build
