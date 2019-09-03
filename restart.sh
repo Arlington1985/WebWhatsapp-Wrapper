2b340cb66955b8678f03aec180a0c5119d175030
Rovshan Musayev <arlington1985@gmail.com>
2019-09-03 15:34:19 +0200

    Added echo

--- restart.sh
+++ restart.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+echo "----------------Started Restart--------------------"
 ./destroy.sh
 sudo cp -TRv firefox_cache_bck firefox_cache
 ./deploy.sh
+echo "----------------Ended restart-----------------------"
