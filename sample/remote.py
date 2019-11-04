60582c1ef753f1b0fb5090d5a9088d20d65413b3
Rovshan Musayev <arlington1985@gmail.com>
2019-11-04 02:04:20 +0100

    Fixed race condition while creating folder and logging added

--- sample/remote.py
+++ sample/remote.py
@@ -1,18 +1,20 @@
-import os, sys, time, json, filecmp
+import os, sys, time, json, filecmp, logging
 from webwhatsapi import WhatsAPIDriver
 from webwhatsapi.objects.message import Message, MediaMessage
 
-print ("Environment", os.environ)
+logging.basicConfig(level=logging.DEBUG)
+
+logging.info ("Environment", os.environ)
 try:
    os.environ["SELENIUM"]
 except KeyError:
-   print("Please set the environment variable SELENIUM to Selenium URL")
+   logging.info("Please set the environment variable SELENIUM to Selenium URL")
    sys.exit(1)
 
 try:
    os.environ["MOBILE_NUMBER"]
 except KeyError:
-   print("Please set the mobile number variable")
+   logging.info("Please set the mobile number variable")
    sys.exit(1)
 
 ##Save session on "/firefox_cache/localStorage.json".
@@ -25,68 +27,66 @@ if not os.path.exists(profiledir):
 
 driver = WhatsAPIDriver(profile=profiledir, client='remote', command_executor=os.environ["SELENIUM"])
 try:
-    print("Waiting for QR")
+    logging.info("Waiting for QR")
     driver.wait_for_login(timeout=9999999999)
-    print("Saving session")
+    logging.info("Saving session")
     driver.save_firefox_profile(remove_old=True)
-    print("Bot started")
+    logging.info("Bot started")
 
 
     while True:
         time.sleep(3)
-        print('Checking for more messages, status, '+ driver.get_status())
+        logging.info('Checking for more messages, status, '+ driver.get_status())
         for contact in driver.get_unread(use_unread_count=True, fetch_all_as_unread=True):
-            print(contact)
+            logging.info(contact)
             for message in contact.messages:
-                print ('class', message.__class__.__name__)
-                print ('message', message)
-                print ('id', message.id)
-                print ('type', message.type)
-                print ('timestamp', message.timestamp)
-                print ('chat_id', message.chat_id)
-                print ('sender', message.sender)
-                print ('sender.id', message.sender.id)
-                print ('sender.safe_name', message.sender.get_safe_name())
+                logging.info ('class', message.__class__.__name__)
+                logging.info ('message', message)
+                logging.info ('id', message.id)
+                logging.info ('type', message.type)
+                logging.info ('timestamp', message.timestamp)
+                logging.info ('chat_id', message.chat_id)
+                logging.info ('sender', message.sender)
+                logging.info ('sender.id', message.sender.id)
+                logging.info ('sender.safe_name', message.sender.get_safe_name())
                 if message.type == 'chat':
-                    print ('-- Chat')
-                    print ('safe_content', message.safe_content)
-                    print ('content', message.content)
+                    logging.info ('-- Chat')
+                    logging.info ('safe_content', message.safe_content)
+                    logging.info ('content', message.content)
                     # contact.chat.send_message(message.safe_content)
                 elif message.type == 'image' or message.type == 'video' :
-                    print ('-- Image or Video')
-                    print ('filename', message.filename)
-                    print ('size', message.size)
-                    print ('mime', message.mime)
-                    print ('caption', message.caption)
-                    print ('client_url', message.client_url)
+                    logging.info ('-- Image or Video')
+                    logging.info ('filename', message.filename)
+                    logging.info ('size', message.size)
+                    logging.info ('mime', message.mime)
+                    logging.info ('caption', message.caption)
+                    logging.info ('client_url', message.client_url)
 
                     # Creating directory tree
                     dirName=os.path.join("/wphotos", message.chat_id['user'][:12])
                     tmp_dir=os.path.join(dirName,"tmp")
-                    if not os.path.exists(dirName):
-                        os.mkdir(dirName)
-                        print("Directory " , dirName ,  " created ")
-                        os.mkdir(tmp_dir)
-                        print("Directory " , tmp_dir ,  " created ")
+                    
+                    if not os.path.exists(tmp_dir):
+                        # Will create dirName and tmp_dir in one shoot
+                        # exist_ok=True, because of paralel execution in another container, to avoid race condition 
+                        os.makedirs(tmp_dir, exist_ok=True)
+                        logging.info("Directory set " , tmp_dir ,  " was created ")
                     else:
-                        print("Directory " , dirName , " already exists")
-                        if not os.path.exists(tmp_dir):
-                            os.mkdir(tmp_dir)
-                            print("Directory " , tmp_dir ,  " created ")
+                        logging.info("Directory set " , tmp_dir , " already exists")
 
                     # Downloading file
                     try:
                         tmp_file=message.save_media(tmp_dir, force_download = True)
                     except Exception as ex:
-                        print("Cannot download photo, skipping")
+                        logging.error("Cannot download photo, skipping")
                         continue
 
                     #driver.delete_message(contact.chat.id,message)
-                    print("Photo downloaded to ",tmp_file)
-                    print("Comparing with old photos")
+                    logging.info("Photo downloaded to ",tmp_file)
+                    logging.info("Comparing with old photos")
                     old_files=[f for f in os.listdir(dirName) if os.path.isfile(os.path.join(dirName, f))]
                     if old_files:
-                        print(old_files)
+                        logging.info(old_files)
                         dublicated=False
                         dublicated_with=[]
                         for old_file in old_files:
@@ -97,19 +97,19 @@ try:
                                 
                         if dublicated:
                             os.remove(tmp_file)
-                            print("Photo duplicated with "+','.join(dublicated_with)+", removed")
+                            logging.info("Photo duplicated with "+','.join(dublicated_with)+", removed")
                         else:
                             os.rename(tmp_file, os.path.join(dirName, os.path.basename(tmp_file)))
-                            print("Photo moved to permanent location")
+                            logging.info("Photo moved to permanent location")
                     else:
                         os.rename(tmp_file, os.path.join(dirName, os.path.basename(tmp_file)))
-                        print("First download, photo moved to permanent location")
+                        logging.info("First download, photo moved to permanent location")
                     #contact.chat.send_message("Photo received")
                 else:
-                    print ('-- Other')
+                    logging.info ('-- Other')
             contact.chat.send_seen()
-            print("Sent seen request")
+            logging.info("Sent seen request")
 except Exception as e:
-	print('EXCEPTION:',e.__class__.__name__,e)
+	logging.exception(e.__class__.__name__,e)
 	driver.close()
 	raise
