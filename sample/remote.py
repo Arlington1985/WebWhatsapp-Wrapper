5d50657f84d91542b37ebb56b7147f7331d85770
Rovshan Musayev <arlington1985@gmail.com>
2020-03-28 15:08:12 +0100

    Fix

--- sample/remote.py
+++ sample/remote.py
@@ -75,7 +75,7 @@ try:
             logging.info(contact)
 
             with db_conn.cursor() as cur:
-                cur.execute(loaded_contacts, (str(mobile_number), str(contact.chat.id)))
+                cur.execute(loaded_contacts, (str(mobile_number), str(contact.chat.id), ))
                 earlier_messages_set=cur.fetchone()
 
             print(str(contact.chat.id))
@@ -111,7 +111,7 @@ try:
                 logging.info ('sender.safe_name: '+ str(message.sender.get_safe_name()))
                 
                 with db_conn.cursor() as cur:
-                    cur.execute(check_if_processed, (str(message.id)))
+                    cur.execute(check_if_processed, (str(message.id), ))
                     result_set=cur.fetchone()
 
                 if result_set is None: 
