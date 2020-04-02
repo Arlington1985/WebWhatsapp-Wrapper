a1adca80c07b22e6f5dee8647ae6b9ef465b1a22
Rovshan Musayev <arlington1985@gmail.com>
2020-04-03 01:03:04 +0200

    sendseen

--- sample/remote.py
+++ sample/remote.py
@@ -211,7 +211,8 @@ try:
                     cur.execute(deactivate_reload, (reload_contact_row_id, ))
                     db_conn.commit()                    
                 logging.info("Reloading deactivated for "+str(reload_contact_row_sender))
-                driver.close()
+                chat.send_seen()
+
 
         else:
             logging.debug("Nothing to reload, continue")
